7edde67fe12cb7749077574a682baf4712d1e865
omelendrez <omar.melendrez@gmail.com>
2019-05-18 16:28:45 -0300

    Crear Header (con NavBar), Footer y Home  page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="fixed-bottom bg-dark text-white">
+      <div className="container">
+        <p className="lead">Donar Ya! - &reg; 2019 - Todos los derechos reservados</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Donar Ya!</h1>
+        <p className="lead">Para que podamos ayudar entre todos.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
