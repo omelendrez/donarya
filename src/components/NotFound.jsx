cfd05f38d219e57237b0e118f7f6688328c92ace
omelendrez <omar.melendrez@gmail.com>
2019-05-21 00:06:18 -0300

    Hace botón más grande

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,7 +6,7 @@ const Notfound = () => {
     <div className="container text-center p-5">
       <h1 className="text-danger my-5">OOPS!</h1>
       <p className="lead my-5">La página que está buscando no existe</p>
-      <Link className="btn btn-danger btn-sm" to="/">Volver a Home</Link>
+      <Link className="btn btn-danger btn-lg" to="/">Volver a Home</Link>
     </div>
   )
 }
