472ff38312848bc73db8ddad7005989613b85f76
frustratedcup <ciediva@gmail.com>
2019-05-23 17:20:51 -0300

    Traducir form de login y agregar form de registro.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,13 +5,13 @@ const Login = () => {
         <div className="container">
             <form>
                 <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar email" />
+                    <label htmlFor="inputEmail">Email</label>
+                    <input type="email" className="form-control" id="inputEmail1" aria-describedby="emailHelp" placeholder="Ingresar email" />
                     <small id="emailHelp" className="form-text text-muted">Nunca compartiremos el email con otra persona.</small>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Constrasena</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Ingresar contrasena" />
+                    <label htmlFor="inputPassword1">Constrasena</label>
+                    <input type="password" className="form-control" id="inputPassword1" placeholder="Ingresar contrasena" />
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
