34fdc72ba6f9cb9168bae92e5a86bb9a2deb0e36
omelendrez <omar.melendrez@gmail.com>
2019-05-24 14:26:16 -0300

    Place redirect before serverStatic

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const app = express()
-const path = require('path')
-app.use(serveStatic(path.join(__dirname, '/build')))
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') {
@@ -12,6 +10,8 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+const path = require('path')
+app.use(serveStatic(path.join(__dirname, '/build')))
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port) // eslint-disable-line no-console
