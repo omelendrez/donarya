ef761fdb41d28eba7cdcd7bbbcb590d203403a78
Emarengo73 <emarengo73@gmail.com>
2019-05-23 21:27:23 -0300

    Aca esta

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className="container">Registrarse</div>
+    <div className="container">
+
+      <form>
+        <div className="form-row">
+          <div className="col">
+            <label htmlFor="inputNick">Nickname</label>
+            <input type="text" className="form-control" placeholder="Sobrenombre" />
+          </div>
+          <div className="col">
+            <label htmlFor="inputName">Name</label>
+            <input type="text" className="form-control" placeholder="Nombre" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputDni">DNI</label>
+          <input type="text" className="form-control" id="inputDni" placeholder="Número de Documento" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputPhone">Phone</label>
+          <input type="text" className="form-control" id="inputPhone" placeholder="Número De Teléfono" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputAddress">Adress</label>
+          <input type="text" className="form-control" id="inputAddress" placeholder="Dirección" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputEmail">Email</label>
+          <input type="text" className="form-control" id="inputAddress" placeholder="Dirección De Email" />
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="inputPassword">Password</label>
+            <input type="email" className="form-control" id="inputPassword" placeholder="Contraseña" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="inputRetypepassword">Retype Password</label>
+            <input type="Retypepassword" className="form-control" id="inputRetypepassword" placeholder="Reescribir Contraseña" />
+          </div>
+
+        </div>
+
+        <button type="submit" className="btn btn-primary">Registrarse</button>
+
+      </form>
+    </div>
   )
 }
 
