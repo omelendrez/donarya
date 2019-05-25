5a684a8c343cca3d42807c9b3a67999556970608
omelendrez <omar.melendrez@gmail.com>
2019-05-24 22:20:07 -0300

    Agregar hooks al login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+  
   return (
-    <div className="container">
-      <form>
+    <div className="container mt-4 mb-5 pb-5">
+      <h4 className="mb-4">Login</h4>
+      <form onSubmit={handleSubmit}>
 
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          <label htmlFor="email">Email {email}</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            placeholder="Ingresá tu email" />
+          <small id="emailHelp" className="form-text text-muted">Nunca vamos a compartir tu email con nadie.</small>
         </div>
 
         <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <label htmlFor="password">Contraseña {password}</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Ingresá tu contraseña"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </div>
 
-        <button type="submit" className="btn btn-primary">Login</button>
+        <div className="form-row">
+          <button
+            type="submit"
+            className="btn btn-success btn-block offset-4 col-4 col-xs-12"
+          >Ingresar</button>
+        </div>
 
       </form>
+
     </div>
   )
 }
