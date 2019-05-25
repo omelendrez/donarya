9ce149cceb2489f2674ed5e2e076413f17a961a5
omelendrez <omar.melendrez@gmail.com>
2019-05-25 19:14:31 -0300

    Iprovements for PWA

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './home.css'
 
 const Home = () => {
   return (
-    <div className="jumbotron">
+    <div className="home pt-4">
       <div className="container">
         <h1 className="display-4">Donar Ya!</h1>
         <p className="lead">Para que podamos ayudar entre todos.</p>
