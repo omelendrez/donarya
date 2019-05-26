05ed33659ef681e9d1cc879f3df19753267f614f
Emarengo73 <emarengo73@gmail.com>
2019-05-26 20:54:26 -0300

    cambios

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,49 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [nick, setNick] = useState('')
+  const [name, setName] = useState('')
+  const [dni, setDni] = useState('')
+  const [phone, setPhone] = useState('')
+  const [adress, setAdress] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password2, setPassword2] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(nick, name, dni, phone, adress, email, password, password2)
+  }
+
   return (
     <div className="container">
 
       <form>
         <div className="form-row">
           <div className="col">
-            <label htmlFor="inputNick">Nickname</label>
-            <input type="text" className="form-control" placeholder="Sobrenombre" />
+            <label htmlFor="nick">Nickname</label>
+            <input type="text" className="form-control" id="nick" placeholder="Sobrenombre" value={nick}
+            onChange={e => setNick(e.target.value)}/>
           </div>
           <div className="col">
-            <label htmlFor="inputName">Name</label>
-            <input type="text" className="form-control" placeholder="Nombre" />
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" id="name" placeholder="Nombre" value={name}
+            onChange={e => setName(e.target.value)}/>
           </div>
         </div>
         <div className="form-group">
-          <label htmlFor="inputDni">DNI</label>
-          <input type="text" className="form-control" id="inputDni" placeholder="Número de Documento" />
+          <label htmlFor="dni">DNI</label>
+          <input type="text" className="form-control" id="dni" placeholder="Número de Documento" value={dni}
+            onChange={e => setDni(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="inputPhone">Phone</label>
-          <input type="text" className="form-control" id="inputPhone" placeholder="Número De Teléfono" />
+          <label htmlFor="phone">Phone</label>
+          <input type="text" className="form-control" id="phone" placeholder="Número De Teléfono" value={phone}
+            onChange={e => setPhone(e.target.value)}  />
         </div>
         <div className="form-group">
-          <label htmlFor="inputAddress">Adress</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="Dirección" />
+          <label htmlFor="address">Adress</label>
+          <input type="text" className="form-control" id="address" placeholder="Dirección" value={adress}
+            onChange={e => setAdress(e.target.value)}/>
         </div>
         <div className="form-group">
-          <label htmlFor="inputEmail">Email</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="Dirección De Email" />
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" placeholder="Dirección De Email" value={email}
+            onChange={e => setEmail(e.target.value)}/>
         </div>
         <div className="form-row">
           <div className="form-group col-md-6">
-            <label htmlFor="inputPassword">Password</label>
-            <input type="email" className="form-control" id="inputPassword" placeholder="Contraseña" />
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Contraseña" value={password}
+            onChange={e => setPassword(e.target.value)}/>
           </div>
           <div className="form-group col-md-6">
-            <label htmlFor="inputRetypepassword">Retype Password</label>
-            <input type="Retypepassword" className="form-control" id="inputRetypepassword" placeholder="Reescribir Contraseña" />
+            <label htmlFor="password2">Retype Password</label>
+            <input type="password" className="form-control" id="password2" placeholder="Reescribir Contraseña" value={password2}
+            onChange={e => setPassword2(e.target.value)}/>
           </div>
 
         </div>
 
-        <button type="submit" className="btn btn-primary">Registrarse</button>
+        <button type="submit" className="btn btn-primary btn-block">Registrarse</button>
 
       </form>
     </div>
