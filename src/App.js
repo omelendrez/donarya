620c3a177346ae6497569c6e43489b116e57b9a4
omelendrez <omar.melendrez@gmail.com>
2019-05-27 20:13:08 -0300

    Refactor routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Route, Switch } from "react-router-dom";
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './components/Home'
+import Donaciones from './components/Donaciones'
+import NotFound from './components/NotFound'
+import Login from './components/Login'
+import Register from './components/Register'
 import './app.scss'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/donaciones/" component={Donaciones} />
+        <Route path="/login/" component={Login} />
+        <Route path="/registrarse/" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
       <Footer />
     </div>
   )
