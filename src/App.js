e27f3ec5673a104987e89ebc44d27504d6567739
omelendrez <omar.melendrez@gmail.com>
2019-05-27 13:37:26 -0300

    Implement SASS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import './app.css'
+import './app.scss'
 
 const App = () => {
   return (
