9e69192f644faeb526964922799061eb88d87635
omelendrez <omar.melendrez@gmail.com>
2019-05-27 21:06:54 -0300

    Optimize colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="fixed-bottom bg-dark text-white text-center">
+    <div className="fixed-bottom text-center">
       <div className="container">
         <p className="lead small pt-2">&copy; 2019 - Donar Ya! - Todos los derechos reservados</p>
       </div>
