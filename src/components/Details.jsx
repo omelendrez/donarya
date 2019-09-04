ba52cfba7828edd67fb6e51d9893d42e9760f1ab
omelendrez <omar.melendrez@gmail.com>
2019-09-03 23:06:07 -0300

    Update 03-09

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-export const Details = (props) => {
-  const { record } = props
-  console.log(record)
+export const Details = ({ record }) => {
+  const { description, user, donation_items } = record
+  if (!user) return null
+  const { fullName, dni, cuit, phone, address } = user
   return (
-    <div>
-      {record.description}
-    </div>
+    <React.Fragment>
+      <div>{description}</div>
+      <div>{fullName}</div>
+      <div>{dni}</div>
+      <div>{cuit}</div>
+      <div>{phone}</div>
+      <div>{address}</div>
+      <ul>
+        {donation_items.map(item => {
+          return (<li>{item.title}</li>)
+        })}
+      </ul>
+    </React.Fragment>
   )
 }
